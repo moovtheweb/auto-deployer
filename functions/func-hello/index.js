a195bc15f9f80c480d16bf2ac0098c957ef43126
moovtheweb <moovtheweb@gmail.com>
2020-03-13 02:35:35 +0000

    test function to auto deploy

---
+++ functions/func-hello/index.js
@@ -0,0 +1,10 @@
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+
+exports.hellohttp = function hellohttp (req, res) {
+  res.send(`Hello ${req.body.name || 'World'}! :-P `);
+};
