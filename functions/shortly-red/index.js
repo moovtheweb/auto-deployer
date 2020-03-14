70b6eb5bdc0ef5afc9e568443e63f000d4ecb92f
moovtheweb <moovtheweb@gmail.com>
2020-03-14 15:19:06 +0000

    redirect service shortly-red

---
+++ functions/shortly-red/index.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const { Datastore } = require('@google-cloud/datastore');
+
+// Creates a client
+const datastore = new Datastore();
+console.log("DS connected => " + datastore);
+const app-red = express()
+const port = process.env.PORT || 3000
+console.log("port => " + port)
+app-red.listen(port, () => {
+    console.log('app is listening')
+})
+
+app-red.use(express.json())
+
+app-red.use(function (req, res, next) {
+    console.log("before");
+    next();
+});
+
+app-red.get("/app/:shorturl",async (req,res,next)=>{
+    console.log("shorturl ==> " + req.params.shorturl)
+    const shorturl = req.params.shorturl
+
+    try{
+        //const surl = await ShortURL.findOne({shorturl:shorturl})
+        const query = datastore.createQuery('ShortURL').filter('shortURL', '=', shorturl);
+        const [surls] = await datastore.runQuery(query);
+        console.log(surls+surls.length)
+        if(!surls.length){
+            return res.status(401).send()
+        }
+        console.log(surls[0].longurl)
+        res.redirect("https://" + surls[0].longurl);
+    } catch (e) {
+        return res.status(500).send()
+    }
+    next();
+})
+
+app-red.use(function (req, res, next) {
+    console.log("after");
+    console.log(req.surl)
+    next();
+});
+
+module.exports = {
+    app-red
+};
